d4c07e6ec162dfd5c3f35e01e11dbf2bf17ede25
chomins <cms4317@naver.com>
2020-11-17 13:09:42 +0900

    수정

--- 민수/Recursion/factorial.py
+++ 민수/Recursion/factorial.py
@@ -1,8 +1,9 @@
 
 def factorial(num):
-    if num==1:
-        return num
+    if num<=1:
+        return 1
     else:
         return num*factorial(num-1)
 
-print(factorial(4))
+num = int(input())
+print(factorial(num))
